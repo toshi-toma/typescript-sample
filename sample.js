cb4002ee174dc2a2688e2281e8d0275071c1182e
toshi-toma <toshitoma1217@gmail.com>
2019-04-17 19:43:01 +0900

    add sample ts file

---
+++ sample.js
@@ -0,0 +1,7 @@
+function isTypeScript(str) {
+    if (str === 'TypeScript') {
+        return true;
+    }
+    return false;
+}
+var typescript = isTypeScript('TypeScript');
---
+++ sample.ts
@@ -0,0 +1,9 @@
+function isTypeScript(str) {
+  if (str === 'TypeScript') {
+    return true
+  }
+  return false
+}
+
+const typescript = isTypeScript('TypeScript');
+
